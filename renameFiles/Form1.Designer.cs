13a9a7c313c45e397ac403c378feb9d9b5e71767
naoki-kkc <naoki2kuchi@gmail.com>
2020-10-28 10:23:04 +0900

    プロジェクト ファイルを追加します。

---
+++ renameFiles/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace RenameCreatedateMd5
+{
+    partial class FileRename
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.renameExecButton = new System.Windows.Forms.Button();
+            this.suffix00x = new System.Windows.Forms.RadioButton();
+            this.dropLabel = new System.Windows.Forms.Label();
+            this.ymdhmsMd5Radio = new System.Windows.Forms.RadioButton();
+            this.renameRuleGroup = new System.Windows.Forms.GroupBox();
+            this.seqRadio = new System.Windows.Forms.RadioButton();
+            this.md5Radio = new System.Windows.Forms.RadioButton();
+            this.ymdRadio = new System.Windows.Forms.RadioButton();
+            this.ymdMd5Radio = new System.Windows.Forms.RadioButton();
+            this.ymdhmsRadio = new System.Windows.Forms.RadioButton();
+            this.suffixGroup = new System.Windows.Forms.GroupBox();
+            this.suffixX = new System.Windows.Forms.RadioButton();
+            this.showLogWindow = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressText = new System.Windows.Forms.Label();
+            this.renameRuleGroup.SuspendLayout();
+            this.suffixGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // renameExecButton
+            // 
+            this.renameExecButton.Enabled = false;
+            this.renameExecButton.Location = new System.Drawing.Point(447, 151);
+            this.renameExecButton.Name = "renameExecButton";
+            this.renameExecButton.Size = new System.Drawing.Size(125, 118);
+            this.renameExecButton.TabIndex = 1;
+            this.renameExecButton.Text = "リネーム実行";
+            this.renameExecButton.UseVisualStyleBackColor = true;
+            this.renameExecButton.Click += new System.EventHandler(this.renameExecButton_Click);
+            // 
+            // suffix00x
+            // 
+            this.suffix00x.AutoSize = true;
+            this.suffix00x.Checked = true;
+            this.suffix00x.Location = new System.Drawing.Point(15, 24);
+            this.suffix00x.Name = "suffix00x";
+            this.suffix00x.Size = new System.Drawing.Size(45, 16);
+            this.suffix00x.TabIndex = 3;
+            this.suffix00x.TabStop = true;
+            this.suffix00x.Text = "_00x";
+            this.suffix00x.UseVisualStyleBackColor = true;
+            // 
+            // dropLabel
+            // 
+            this.dropLabel.AllowDrop = true;
+            this.dropLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dropLabel.Location = new System.Drawing.Point(12, 9);
+            this.dropLabel.Name = "dropLabel";
+            this.dropLabel.Size = new System.Drawing.Size(560, 129);
+            this.dropLabel.TabIndex = 4;
+            this.dropLabel.Text = "リネームするファイルが入ったフォルダをここにドラッグ...";
+            this.dropLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.dropLabel.DragDrop += new System.Windows.Forms.DragEventHandler(this.dropLabel_DragDrop);
+            this.dropLabel.DragEnter += new System.Windows.Forms.DragEventHandler(this.dropLabel_DragEnter);
+            // 
+            // ymdhmsMd5Radio
+            // 
+            this.ymdhmsMd5Radio.Checked = true;
+            this.ymdhmsMd5Radio.Location = new System.Drawing.Point(6, 24);
+            this.ymdhmsMd5Radio.Name = "ymdhmsMd5Radio";
+            this.ymdhmsMd5Radio.Size = new System.Drawing.Size(226, 16);
+            this.ymdhmsMd5Radio.TabIndex = 11;
+            this.ymdhmsMd5Radio.TabStop = true;
+            this.ymdhmsMd5Radio.Text = "作成日時のyyyymmddhhmmss形式_md5";
+            this.ymdhmsMd5Radio.UseVisualStyleBackColor = true;
+            // 
+            // renameRuleGroup
+            // 
+            this.renameRuleGroup.Controls.Add(this.seqRadio);
+            this.renameRuleGroup.Controls.Add(this.md5Radio);
+            this.renameRuleGroup.Controls.Add(this.ymdRadio);
+            this.renameRuleGroup.Controls.Add(this.ymdMd5Radio);
+            this.renameRuleGroup.Controls.Add(this.ymdhmsRadio);
+            this.renameRuleGroup.Controls.Add(this.ymdhmsMd5Radio);
+            this.renameRuleGroup.Location = new System.Drawing.Point(12, 151);
+            this.renameRuleGroup.Name = "renameRuleGroup";
+            this.renameRuleGroup.Size = new System.Drawing.Size(304, 162);
+            this.renameRuleGroup.TabIndex = 12;
+            this.renameRuleGroup.TabStop = false;
+            this.renameRuleGroup.Text = "ファイル名変更ルール";
+            // 
+            // seqRadio
+            // 
+            this.seqRadio.Location = new System.Drawing.Point(6, 134);
+            this.seqRadio.Name = "seqRadio";
+            this.seqRadio.Size = new System.Drawing.Size(226, 16);
+            this.seqRadio.TabIndex = 16;
+            this.seqRadio.Text = "連番(6桁)";
+            this.seqRadio.UseVisualStyleBackColor = true;
+            // 
+            // md5Radio
+            // 
+            this.md5Radio.Location = new System.Drawing.Point(6, 112);
+            this.md5Radio.Name = "md5Radio";
+            this.md5Radio.Size = new System.Drawing.Size(226, 16);
+            this.md5Radio.TabIndex = 15;
+            this.md5Radio.Text = "md5";
+            this.md5Radio.UseVisualStyleBackColor = true;
+            // 
+            // ymdRadio
+            // 
+            this.ymdRadio.Location = new System.Drawing.Point(6, 90);
+            this.ymdRadio.Name = "ymdRadio";
+            this.ymdRadio.Size = new System.Drawing.Size(226, 16);
+            this.ymdRadio.TabIndex = 14;
+            this.ymdRadio.Text = "作成日時のyyyymmdd形式";
+            this.ymdRadio.UseVisualStyleBackColor = true;
+            // 
+            // ymdMd5Radio
+            // 
+            this.ymdMd5Radio.Location = new System.Drawing.Point(6, 68);
+            this.ymdMd5Radio.Name = "ymdMd5Radio";
+            this.ymdMd5Radio.Size = new System.Drawing.Size(226, 16);
+            this.ymdMd5Radio.TabIndex = 13;
+            this.ymdMd5Radio.Text = "作成日時のyyyymmdd形式_md5";
+            this.ymdMd5Radio.UseVisualStyleBackColor = true;
+            // 
+            // ymdhmsRadio
+            // 
+            this.ymdhmsRadio.Location = new System.Drawing.Point(6, 46);
+            this.ymdhmsRadio.Name = "ymdhmsRadio";
+            this.ymdhmsRadio.Size = new System.Drawing.Size(226, 16);
+            this.ymdhmsRadio.TabIndex = 12;
+            this.ymdhmsRadio.Text = "作成日時のyyyymmddhhmmss形式";
+            this.ymdhmsRadio.UseVisualStyleBackColor = true;
+            // 
+            // suffixGroup
+            // 
+            this.suffixGroup.Controls.Add(this.suffixX);
+            this.suffixGroup.Controls.Add(this.suffix00x);
+            this.suffixGroup.Location = new System.Drawing.Point(322, 151);
+            this.suffixGroup.Name = "suffixGroup";
+            this.suffixGroup.Size = new System.Drawing.Size(119, 162);
+            this.suffixGroup.TabIndex = 13;
+            this.suffixGroup.TabStop = false;
+            this.suffixGroup.Text = "重複時のサフィックス";
+            // 
+            // suffixX
+            // 
+            this.suffixX.AutoSize = true;
+            this.suffixX.Location = new System.Drawing.Point(15, 46);
+            this.suffixX.Name = "suffixX";
+            this.suffixX.Size = new System.Drawing.Size(33, 16);
+            this.suffixX.TabIndex = 4;
+            this.suffixX.Text = "_x";
+            this.suffixX.UseVisualStyleBackColor = true;
+            // 
+            // showLogWindow
+            // 
+            this.showLogWindow.Enabled = false;
+            this.showLogWindow.Location = new System.Drawing.Point(447, 275);
+            this.showLogWindow.Name = "showLogWindow";
+            this.showLogWindow.Size = new System.Drawing.Size(125, 36);
+            this.showLogWindow.TabIndex = 14;
+            this.showLogWindow.Text = "実行ログ表示";
+            this.showLogWindow.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 320);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(560, 23);
+            this.progressBar.TabIndex = 15;
+            // 
+            // progressText
+            // 
+            this.progressText.Location = new System.Drawing.Point(448, 320);
+            this.progressText.Name = "progressText";
+            this.progressText.Size = new System.Drawing.Size(124, 23);
+            this.progressText.TabIndex = 16;
+            this.progressText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FileRename
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 352);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.showLogWindow);
+            this.Controls.Add(this.suffixGroup);
+            this.Controls.Add(this.renameRuleGroup);
+            this.Controls.Add(this.dropLabel);
+            this.Controls.Add(this.renameExecButton);
+            this.Controls.Add(this.progressText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FileRename";
+            this.Text = "renameFiles";
+            this.renameRuleGroup.ResumeLayout(false);
+            this.suffixGroup.ResumeLayout(false);
+            this.suffixGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button renameExecButton;
+        private System.Windows.Forms.RadioButton suffix00x;
+        private System.Windows.Forms.Label dropLabel;
+        private System.Windows.Forms.RadioButton ymdhmsMd5Radio;
+        private System.Windows.Forms.GroupBox renameRuleGroup;
+        private System.Windows.Forms.GroupBox suffixGroup;
+        private System.Windows.Forms.RadioButton ymdRadio;
+        private System.Windows.Forms.RadioButton ymdMd5Radio;
+        private System.Windows.Forms.RadioButton ymdhmsRadio;
+        private System.Windows.Forms.RadioButton suffixX;
+        private System.Windows.Forms.Button showLogWindow;
+        private System.Windows.Forms.RadioButton seqRadio;
+        private System.Windows.Forms.RadioButton md5Radio;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label progressText;
+    }
+}
+
---
+++ renameFiles/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenameCreatedateMd5
+{
+    public partial class FileRename : Form
+    {
+        private string dropedPath = "";
+
+        public FileRename()
+        {
+            InitializeComponent();
+        }
+
+        private void renameExecButton_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("clicked!");
+            string[] files = Directory.GetFiles(dropedPath, "*");
+
+            this.progressBar.Minimum = 0;
+            this.progressBar.Maximum = files.Length;
+            this.progressBar.Value = 0;
+
+            int current = 0;
+            try
+            {
+                foreach (string sourcePath in files)
+                {
+                    current++;
+
+                    // 変更後のファイル名を作成する
+                    FileInfo sourceFileInfo = new FileInfo(sourcePath);
+                    StringBuilder changeNameBuilder = new StringBuilder();
+
+                    // yyyymmddhhmmss系
+                    if (this.ymdhmsMd5Radio.Checked || this.ymdhmsRadio.Checked)
+                    {
+                        changeNameBuilder.Append(sourceFileInfo.CreationTime.ToString("yyyyMMddHHmmss"));
+                    }
+                    if (this.ymdhmsMd5Radio.Checked)
+                    {
+                        changeNameBuilder.Append("_");
+                        changeNameBuilder.Append(calcMd5Hash(sourcePath));
+                    }
+
+                    // yyyymmdd系
+                    if (this.ymdRadio.Checked || this.ymdMd5Radio.Checked)
+                    {
+                        changeNameBuilder.Append(sourceFileInfo.CreationTime.ToString("yyyyMMdd"));
+                    }
+                    if (this.ymdMd5Radio.Checked)
+                    {
+                        changeNameBuilder.Append("_");
+                        changeNameBuilder.Append(calcMd5Hash(sourcePath));
+                    }
+
+                    // md5
+                    if (this.md5Radio.Checked)
+                    {
+                        changeNameBuilder.Append(calcMd5Hash(sourcePath));
+                    }
+
+                    // 連番
+                    if (this.seqRadio.Checked)
+                    {
+                        changeNameBuilder.Append(current.ToString("D6"));
+                    }
+
+                    // 拡張子を除いたパスを生成
+                    string destPathWithoutExt = Path.Combine(sourceFileInfo.DirectoryName, changeNameBuilder.ToString());
+
+                    // 拡張子を取得
+                    string ext = Path.GetExtension(sourcePath);
+
+                    // 重複処理
+                    string destPath = destPathWithoutExt + ext;
+                    if (File.Exists(destPathWithoutExt + ext))
+                    {
+                        int cnt = 0;
+                        while (true)
+                        {
+                            cnt++;
+
+                            string cntStr = "_";
+                            if (this.suffix00x.Checked)
+                            {
+                                cntStr += cnt.ToString("D3");
+                            }
+                            if (this.suffixX.Checked)
+                            {
+                                cntStr += cnt.ToString();
+                            }
+
+                            destPath = destPathWithoutExt + cntStr + ext;
+
+                            if (!File.Exists(destPath))
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    File.Move(sourcePath, destPath);
+
+                    this.progressBar.Value = current;
+                    //updateProgressLabel(current, files.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("エラーが発生しました\n"+ex.Message);
+            }
+
+            MessageBox.Show("処理が完了しました");
+        }
+
+        //private void updateProgressLabel(int current, int maximum)
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    sb.Append(current.ToString());
+        //    sb.Append("/");
+        //    sb.Append(maximum.ToString());
+        //    this.progressText.Text = sb.ToString();
+        //}
+
+        private string calcMd5Hash(string filepath)
+        {
+            FileStream fs = null;
+            MD5CryptoServiceProvider md5 = null;
+            byte[] bs = null;
+            try
+            {
+                fs  = new FileStream(filepath, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read);
+                md5 = new MD5CryptoServiceProvider();
+                bs  = md5.ComputeHash(fs);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+                if (md5 != null)
+                {
+                    md5.Clear();
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            foreach (byte b in bs)
+            {
+                result.Append(b.ToString("x2"));
+            }
+
+            return result.ToString();
+        }
+
+        private void dropLabel_DragEnter(object sender, DragEventArgs e)
+        {
+            // ラベルにドラッグされた時にドロップ可能にする
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        //private void label1_DragOver(object sender, DragEventArgs e)
+        //{
+        //    //Console.WriteLine("dragover!");
+        //}
+
+        private void dropLabel_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドロップされた時に行う処理
+            dropedPath = "";
+            this.dropLabel.Text = "リネームするファイルが入ったフォルダをここにドラッグ...";
+            this.renameExecButton.Enabled = false;
+
+            // ドロップされたファイルパスを取得
+            string[] dropElements = (string[])e.Data.GetData(DataFormats.FileDrop);
+            
+            if (1 < dropElements.Length)
+            {
+                // ファイルが複数だった場合は処理対象外
+                MessageBox.Show("ドロップするフォルダは1つです");
+            }
+            else
+            {
+                string tmpPath = dropElements[0];
+
+                if (File.Exists(tmpPath))
+                {
+                    // ファイルだった場合は処理対象外
+                    MessageBox.Show("ファイルが格納されている親フォルダをドロップしてください");
+                }
+                else if (Directory.Exists(tmpPath))
+                {
+                    // ファイルが複数だった場合は処理対象外
+                    dropedPath = dropElements[0];
+                    this.dropLabel.Text = dropedPath;
+
+                    // 実行ボタン有効化
+                    this.renameExecButton.Enabled = true;
+                }
+
+            }
+            
+        }
+    }
+}
